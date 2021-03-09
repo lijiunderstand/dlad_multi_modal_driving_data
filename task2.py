3f8c24948f1a805736c6285f76d031795c44faeb
corkillj <corkillj@student.ethz.ch>
2021-03-09 23:22:16 +0100

    complete 2.2

--- task2.py
+++ task2.py
@@ -59,7 +59,7 @@ plt.ylim(376,0)
 plt.xlim(0,1241) 
 
 plt.savefig("Velodyne_Projected.png", dpi= 1000)
-
+plt.show() 
 
 """--------------------------
 Do 3D bounding Boxes for Cars
@@ -70,36 +70,42 @@ objects = data["objects"] #this has to be one of the dumbest data structures...
 for obj in objects:
     location3D_0 = obj[11:14] #3d location in frame 0
     obj_size = obj[8:11]
-    rot_y = obj[14] + np.pi/2
+    rot_y = obj[14] 
     
     """ find Location of corners """
-    x = obj_size[0]/2 + 1
-    y = obj_size[1]
-    z = obj_size[2]/2
+    x = obj_size[2]/2
+    y = obj_size[0]
+    z = obj_size[1]/2
     # !!!! WEIRD ASS COORDINATES FOR CAM 0
     corners_car_frame = np.array([[x, -y, z],
                                   [-x, -y, z],
                                   [-x, -y, -z],
                                   [x, -y, -z],
-                                  [-x, 0, z],
                                   [x, 0, z],
+                                  [-x, 0, z],
                                   [-x, 0, -z],
                                   [x, 0, -z]])
     rot = np.array([[np.cos(rot_y), 0, np.sin(rot_y)],
                     [0, 1, 0], 
                     [-np.sin(rot_y), 0, np.cos(rot_y)]])
     corners_0 = (rot @ corners_car_frame.T).T + location3D_0
-    corners_2 = corners_0 + np.array([0.6,0,0]) 
+    corners_2 = corners_0 
     
     """ Project corners and lines onto Image """
-    
+    color_box = np.array(list(np.random.choice(range(256), size=3))) /255.0
     corners_uv2_lambda = P_rect_20 @ np.concatenate((corners_2 , np.ones((8,1))), axis =1).T 
     corners_uv2 = corners_uv2_lambda / corners_uv2_lambda[2,:]
-    plt.scatter(corners_uv2[0,:], corners_uv2[1,:], s=2)
-    #plt.plot(corners_uv2[0,:], corners_uv2[1,:] , linewidth=0.5)
-    #plt.plot(corners_uv2[0,1:3], corners_uv2[1,1:3])
-    #plt.plot(corners_uv2[0,2:4], corners_uv2[1,2:4])
-    #plt.plot(corners_uv2[0,2:4], corners_uv2[1,2:4])
+    plt.scatter(corners_uv2[0,:], corners_uv2[1,:], s=0.5, color = color_box)
+    plt.plot(corners_uv2[0,0:4], corners_uv2[1,0:4] , linewidth=0.5, color = color_box)
+    plt.plot(corners_uv2[0,[0,3]], corners_uv2[1,[0,3]] , linewidth=0.5, color = color_box)
+    plt.plot(corners_uv2[0,4:8], corners_uv2[1,4:8] , linewidth=0.5, color = color_box)
+    plt.plot(corners_uv2[0,[4,7]], corners_uv2[1,[4,7]] , linewidth=0.5, color = color_box)
+    plt.plot(corners_uv2[0,[0,4]], corners_uv2[1,[0,4]] , linewidth=0.5, color = color_box)
+    plt.plot(corners_uv2[0,[1,5]], corners_uv2[1,[1,5]] , linewidth=0.5, color = color_box)
+    plt.plot(corners_uv2[0,[2,6]], corners_uv2[1,[2,6]] , linewidth=0.5, color = color_box)
+    plt.plot(corners_uv2[0,[3,7]], corners_uv2[1,[3,7]] , linewidth=0.5, color = color_box)
+    
+    
     
     
 plt.savefig("Velodyne_Projected_3DBox.png", dpi= 1000)
