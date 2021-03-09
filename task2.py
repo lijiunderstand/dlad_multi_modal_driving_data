84752c31252a12b7204d83512f141c095029233f
corkillj <corkillj@student.ethz.ch>
2021-03-09 13:22:30 +0100

    scatterplot works,todo sematic label

--- task2.py
+++ task2.py
@@ -26,14 +26,24 @@ velodyne = data["velodyne"]
 K_cam2 = data["K_cam2"]
 T_cam2_velo = data["T_cam2_velo"]
 P_rect_20 = data["P_rect_20"]
+sem_label = data["sem_label"]
 
 """filter points"""
 # Idea, just roughly filter points behind the car, dont mind points outside the field of view
 # this should insure that we dont have wrong projections
-#velodyne_filted = np.where(velodyne )
-#velo_filtered = np.copy(velodyne)
-#velo_filtered[velo_filtered[:,0]<0] = np.zeros((1,4))
-#velo_filtered = velodyne[~np.all(velo_filtered == 0, axis = 1)]
 velo_filtered = velodyne[velodyne[:,0]>0]
-points_C = T_cam2_velo @ velodyne.T # @ is shorthand for np.matmult
+
+"""project points"""
+points_C = T_cam2_velo @ velo_filtered.T # @ is shorthand for np.matmult
 uv_img_cords =  K_cam2 @ points_C[0:3,:] / points_C[2,:]
+
+#fig, ax = plt.subplots()
+plt.imshow(img_cam_2)
+plt.scatter(uv_img_cords[0,:], uv_img_cords[1,:], s = 1, marker = '.', edgecolors = 'none')
+plt.ylim(376,0)
+plt.xlim(0,1241) 
+
+plt.savefig("Velodyne_Projected.png", dpi= 2000)
+#plt.scatter(, )
+
+plt.show()
